89a6878e13929e32ea991bdf3086ba05353dc77b
YourBright <gurujowa@gmail.com>
2013-07-31 16:19:36 +0900

    new graph

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,9 +1,19 @@
 class GraphsController < ApplicationController
   def index
-    hash = Company.connection.select_all(
+    @day_created = make_day_created
+    @week_created = make_week_created
+  end
+
+  def created
+
+  end
+  
+  private
+  def make_day_created
+     hash = Company.connection.select_all(
     'select strftime("%Y-%m-%d", created_at) m_cre, count(id) as count
      from companies
-     where m_cre != "2013-07-28"
+     where m_cre > "2013-07-28"
      group by m_cre
      order by m_cre desc
      limit 8')
@@ -16,7 +26,7 @@ class GraphsController < ApplicationController
       data.push h["count"]
     end
 
-    @h = LazyHighCharts::HighChart.new("graph") do |f|
+    graph = LazyHighCharts::HighChart.new("graph") do |f|
       f.ｙAxis[
       {title: {text: '登録数'}},
       ]
@@ -26,33 +36,39 @@ class GraphsController < ApplicationController
       f.series(:name => "登録数",
                :data => data)
     end
+    
+    return graph
   end
-
-  def created
-
-    hash = Company.connection.select_all(
-    'select strftime("%Y-%m-%d", created_at) m_cre, count(id) as count
+  
+    private
+  def make_week_created
+     hash = Company.connection.select_all(
+    'select strftime("%Y-%W", created_at) w_cre, count(id) as count
      from companies
-     where m_cre != "2013-07-28"
-     group by m_cre
-     order by m_cre desc')
+     where strftime("%Y-%m-%d", created_at) > "2013-07-28"
+     group by w_cre
+     order by w_cre desc
+     limit 8')
 
     column = []
     data = []
 
     hash.each do |h|
-      column.push Date.parse(h["m_cre"]).strftime('%m/%d(%a)')
+      column.push h["w_cre"]
       data.push h["count"]
     end
 
-    @h = LazyHighCharts::HighChart.new("graph") do |f|
-
+    graph = LazyHighCharts::HighChart.new("graph") do |f|
+      f.ｙAxis[
+      {title: {text: '登録数'}},
+      ]
       f.chart(:type => "column")
-      f.title(:text => "日ごと登録数")
+      f.title(:text => "週ごと登録数")
       f.xAxis(:categories => column)
       f.series(:name => "登録数",
                :data => data)
     end
-
+    
+    return graph
   end
 end
