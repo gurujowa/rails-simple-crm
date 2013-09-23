2e6cade7e0923a67db3fd13c899db959157807cf
root <root@localhost.localdomain>
2013-09-23 13:18:05 +0900

    new checkout

---
+++ lib/inputs/date_picker_input.rb
@@ -0,0 +1,9 @@
+class DatePickerInput < SimpleForm::Inputs::Base
+  def input
+    @builder.text_field(attribute_name, input_options)
+  end
+
+  def input_options
+    { :dateFormat => "yy/mm/dd"}
+  end
+end
