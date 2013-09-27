7c5df6418b65f9563e51a61bf905030f28980b7c
gurujowa <gurujowa@gmail.com>
2013-09-27 11:26:22 +0900

    capistano shared folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,19 @@
 set :application, "rails-simple-crm"
 set :user, "yamashita"
-#set :use_sudo, false
+set :use_sudo, false
 set :repository,  "https://github.com/gurujowa/rails-simple-crm.git"
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-#set :deploy_via,      :copy
 
 
-role :web, "192.168.1.145"
-set :deploy_to, "/var/www/html/crm/"
+role :app, "192.168.1.145"
+set :deploy_to, "/var/www/html/rails-crm/"
 #role :app, "192.168.1.145"
 #role :app, "your app-server here"                          # This may be the same as your `Web` server
 #role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 
-desc 'start capistano OKOK'
-task :hello_capistano do
-  run 'echo "HELLO"'
+
+task :db_setup, :roles => [:db] do
+  run "mkdir -p -m 775 #{shared_path}/db"
 end
 
 # if you want to clean up old releases on each deploy uncomment this:
