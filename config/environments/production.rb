0f39fe2422db3488c9d6eee7c5dbf8676845c326
gurujowa <gurujowa@gmail.com>
2013-12-26 14:57:09 +0900

    pdfkit bug avoid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Mycrm::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "http://deppu.yourbright.co.jp:11223"
+  #config.action_controller.asset_host = "http://deppu.yourbright.co.jp:11223"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
