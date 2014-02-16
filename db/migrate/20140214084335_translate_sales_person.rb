2788b3a36657ddfa79db07282b3dacc2a215e3e2
gurujowa <gurujowa@gmail.com>
2014-02-16 19:12:51 +0900

    migrate changed

--- db/migrate/20140214084335_translate_sales_person.rb
+++ db/migrate/20140214084335_translate_sales_person.rb
@@ -2,7 +2,7 @@ class TranslateSalesPerson < ActiveRecord::Migration
   def up
     companies = Company.all
     companies.each do |c|
-      Nego.create!(name: "新規顧客向け商談", user_id: c.sales_person, stage: c.status_id, company_id: c.id)
+      Nego.create!(name: "新規顧客向け商談", user_id: c.read_attribute(:sales_person), stage: c.status_id, company_id: c.id)
     end
     remove_column :companies, :sales_person
 
