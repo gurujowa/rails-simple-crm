c05dd4a2bf13f744824cbf50aad914f540484b59
gurujowa <gurujowa@gmail.com>
2014-04-04 17:57:11 +0900

    course edit bug fixed

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,7 +6,7 @@ class CoursesController < ApplicationController
   end
   
   def edit
-#    @course = Course.find(params[:id])
+    @course = Course.find(params[:id])
 #    @course.address = @course.company.getAddress if @course.address.blank?
 #    @course.tel = @course.company.tel if @course.tel.blank?
 #    @course.responsible = @course.company.clients.first.name if @course.responsible.blank? and @course.company.clients.present?
