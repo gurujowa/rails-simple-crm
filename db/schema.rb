6ad4cc6d649849f2aded49ed535a2375a08f2c50
gurujowa <gurujowa@gmail.com>
2014-06-26 11:33:36 +0900

    notstart import

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140626001349) do
+ActiveRecord::Schema.define(version: 20140626022346) do
 
   create_table "bill_lines", force: true do |t|
     t.string   "name",                   null: false
