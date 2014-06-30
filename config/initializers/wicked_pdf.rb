abc7fe28b05074c263c63e476128f59ee286d28e
gurujowa <gurujowa@gmail.com>
2014-06-30 12:19:10 +0900

    bug checked production

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,4 +1,13 @@
-WickedPdf.config = {
-  :exe_path => "#{Gem.loaded_specs['wkhtmltopdf-binary'].full_gem_path}/bin/wkhtmltopdf"
-}
+require 'rbconfig'
 
+if RbConfig::CONFIG['host_os'] =~ /linux/
+  arch = RbConfig::CONFIG['host_cpu'] == 'x86_64' ? 'wkhtmltopdf_linux_x64' : 'wkhtmltopdf_linux_386'
+elsif RbConfig::CONFIG['host_os'] =~ /darwin/
+  arch = 'wkhtmltopdf_darwin_386'
+else
+  raise "Invalid platform. Must be running Intel-based Linux or OSX."
+end
+
+WickedPdf.config = {
+  exe_path: "#{ENV['GEM_HOME']}/gems/wkhtmltopdf-binary-#{Gem.loaded_specs['wkhtmltopdf-binary'].version}/bin/#{arch}"
+}
