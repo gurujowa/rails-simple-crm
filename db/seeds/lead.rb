c974b4cecb8ae5a9170a3c4c7be2d01c616d8b02
gurujowa <gurujowa@gmail.com>
2015-06-07 19:40:05 +0900

    rank a db:seed:lead

--- db/seeds/lead.rb
+++ db/seeds/lead.rb
@@ -15,7 +15,7 @@ def tel_encode(input)
     i
 end
 
-table = CSV.table('db/matsudo.csv',encoding: "Windows-31J:UTF-8")
+table = CSV.table('db/nakano.csv',encoding: "Windows-31J:UTF-8")
 table.each do |r|
   if r[:client_name].blank?
     next
@@ -27,13 +27,26 @@ table.each do |r|
     lead.tel =  tel_encode(r[:tel])
     lead.fax = tel_encode(r[:fax])
     lead.tag_list.add(r[:category])
-    lead.url = r[:url]
     #郵便番号
     lead.zipcode.gsub! "〒",""
+    lead.memo = <<EOL
+  カテゴリ：#{r[:category]}
+  管理者：#{r[:client_person]}
+  管理者の役職：#{r[:client_post]}
+  スタッフ数：#{r[:staff]}
+  法人の種類：#{r[:company]}
+  法人名：#{r[:company_name]}
+  法人の住所：#{r[:company_address]}
+  法人の電話番号：#{r[:company_tel]}
+  法人のFAX：#{r[:company_fax]}
+  法人代表者：#{r[:company_person]}
+  法人代表者の役職名：#{r[:company_post]}
+EOL
 
   end
 
     #住所
+    lead.url = r[:url]
     st = r[:address].to_s.split(" ")
     ad = nil
     if st.length == 2
@@ -43,24 +56,13 @@ table.each do |r|
       ad = r[:address]
     end
 
+    lead.tag_list.add("ランクA")
     lead.prefecture = r[:prefecture]
     lead.city = r[:city]
     lead.street = ad
     lead.street.to_s.strip
 
 
-  lead.memo = <<EOL
-カテゴリ：#{r[:category]}
-管理者：#{r[:client_person]}
-管理者の役職：#{r[:client_post]}
-法人の種類：#{r[:company]}
-法人名：#{r[:company_name]}
-法人の住所：#{r[:company_address]}
-法人の電話番号：#{r[:company_tel]}
-法人のFAX：#{r[:company_fax]}
-法人代表者：#{r[:company_person]}
-法人代表者の役職名：#{r[:company_post]}
-EOL
 
     unless lead.save
       p lead.tel
