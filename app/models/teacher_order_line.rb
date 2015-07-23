1eacaebac8d0f26f636c6b25f489494fde15d0f8
gurujowa <gurujowa@gmail.com>
2015-07-23 14:50:16 +0900

    index invoice_date added

--- app/models/teacher_order_line.rb
+++ app/models/teacher_order_line.rb
@@ -3,8 +3,6 @@ class TeacherOrderLine < ActiveRecord::Base
   has_paper_trail 
   belongs_to :teacher_order
 
-  validates :invoice_date, presence: true
-  validates :payment_date, presence: true
   validates :price, presence: true
 
 end
