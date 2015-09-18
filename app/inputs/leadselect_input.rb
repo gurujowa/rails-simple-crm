1a3ff1047cc1fe93a9c2ff7623f197f00db2121b
gurujowa <gurujowa@gmail.com>
2015-09-18 00:14:39 -0700

    estimate new bug fixed

--- app/inputs/leadselect_input.rb
+++ app/inputs/leadselect_input.rb
@@ -1,9 +1,13 @@
 class LeadselectInput < SimpleForm::Inputs::Base
   def input(wrapper_options = nil)
-    lead_id = @builder.object.lead.id
-    name = @builder.object.lead.name
+    default = []
+    if @builder.object.lead.present?
+      lead_id = @builder.object.lead.id
+      name = @builder.object.lead.name
+      default = [[name,lead_id]]
+    end
     merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
     merged_input_options[:class] << "search_lead form-control"
-    out = "#{@builder.select(attribute_name,[[name,lead_id]],{}, merged_input_options)}".html_safe
+    out = "#{@builder.select(attribute_name,default,{}, merged_input_options)}".html_safe
   end
 end
