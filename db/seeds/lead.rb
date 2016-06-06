aa366f542e40f01081ed3115e0033fb54c9d2605
gurujowa <gurujowa@gmail.com>
2016-06-05 21:01:45 -0700

    lead.csv changed

--- db/seeds/lead.rb
+++ db/seeds/lead.rb
@@ -3,56 +3,50 @@ require 'kconv'
 require 'tempfile'
 
 def tel_encode(input)
-    i = input.to_s.gsub "－", "-"
-    i = i.gsub "－","-"
+    i = input.to_s.gsub /[\u30FC]|[\u2010]|[\u2011]|[\u2013]|[\u2014]|[\u2015]|[\u2212]|[\uFF70]/ , "-"
     i = i.gsub "/" , ""
+    i = i.gsub "~" , ""
     i = i.gsub "." , ""
     i = i.gsub "、" , ""
     i = i.gsub /(\d{2,4})[\(（\-\?](\d{2,4})[）\?\-\)](\d{2,4})/ , "\\1-\\2-\\3"
     i = i.gsub "（", ""
     i = i.strip
     i = NKF.nkf('-m0Z1 -w', i)
-    i
+    return i
+end
+
+def create_lead(r)
+  lead = Lead.new(:name => r[:client_name],:zipcode => r[:zip_code])
+  lead.tel =  tel_encode(r[:tel])
+  lead.fax = tel_encode(r[:fax])
+  lead.tag_list.add(r[:category])
+  lead.tag_list.add("テレアポ用リスト")
+  lead.tag_list.add("wamnet #{Time.current.to_s(:dash_date)}追加")
+  lead.zipcode.gsub! "〒",""
+  lead.url = r[:url]
+  puts "new lead added"
+  return lead
 end
 
 table = CSV.table('db/lead_list.csv',encoding: "Windows-31J:UTF-8")
 table.each do |r|
+  #事業所名が空白の場合、スキップ
   if r[:client_name].blank?
     next
   end
+
+  #もし電話番号で発見できない場合、新しく作成
   lead = Lead.find_by(tel: tel_encode(r[:tel]))
+  lead = create_lead(r) if lead.blank?
 
-  if lead.blank?
-    puts "new lead added"
-    lead = Lead.new(:name => r[:client_name],:zipcode => r[:zip_code])
-    lead.tel =  tel_encode(r[:tel])
-    lead.fax = tel_encode(r[:fax])
-    lead.tag_list.add(r[:category])
-    lead.tag_list.add("テレアポ用リスト")
-    #郵便番号
-    lead.zipcode.gsub! "〒",""
+  #住所情報の更新
+  lead.prefecture = r[:prefecture]
+  lead.city = r[:city]
+  lead.street = r[:street]
+  lead.building = r[:building]
 
-
-
-    #住所
-    lead.url = r[:url]
-  end
-
-    st = r[:address].to_s.split(" ")
-    ad = nil
-    if st.length >= 2
-      ad = st[0]
-      lead.building = st[1]
-      lead.building << st[2] if st[2].present?
-    else
-      ad = r[:address]
-    end
-    lead.prefecture = r[:prefecture]
-    lead.city = r[:city]
-    lead.street = ad
-    lead.street.to_s.strip
-
-  lead.memo = <<EOL
+  memo = <<EOL
+  ------- wamnet #{Time.current.to_s(:jp_date)}更新---------------------
   カテゴリ：#{r[:category]}
   管理者：#{r[:client_person]}
   管理者の役職：#{r[:client_post]}
@@ -64,17 +58,17 @@ table.each do |r|
   法人のFAX：#{r[:company_fax]}
   法人代表者：#{r[:company_person]}
   法人代表者の役職名：#{r[:company_post]}
+  ----------------------------------------------------------- 
 EOL
+  lead.memo = lead.memo.present? ? lead.memo << memo : memo
 
   lead.corporation_name =  r[:company_name]
+  lead.tag_list.add("wamnet #{Time.current.to_s(:dash_date)}更新")
 
-
-
-    unless lead.save
-      p lead.tel
-      p lead.prefecture
-      p lead.city
-      p lead.errors.full_messages.inspect
-    end
-
+  unless lead.save
+    p lead.tel
+    p lead.prefecture
+    p lead.city
+    p lead.errors.full_messages.inspect
+  end
 end
